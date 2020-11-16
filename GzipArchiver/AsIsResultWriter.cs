aa6125c18eaf7137073ee6f16f40d2ab41078e50
Denis <igehuc.dev@gmail.com>
2020-11-17 01:34:34 +0300

    AsIsResultWriter

---
+++ GzipArchiver/AsIsResultWriter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+
+namespace GzipArchiver
+{
+    public class AsIsResultWriter : IResultWriter, IDisposable
+    {
+        public AsIsResultWriter(string outputPath)
+            : this(InitStream(outputPath))
+        {
+        }
+
+        // Just an example of DI principle but in practice
+        //   it does not make a lot sense here with Stream class
+        public AsIsResultWriter(Stream outputStream)
+        {
+            _outputStream = outputStream ?? throw new ArgumentNullException(nameof(outputStream));
+        }
+
+        public void WritePortion(int index, MemoryStream portion)
+        {
+            portion.CopyTo(_outputStream);
+        }
+
+        public void Dispose()
+        {
+            _outputStream?.Dispose();
+        }
+
+        private static Stream InitStream(string outputPath)
+        {
+            if (string.IsNullOrWhiteSpace(outputPath))
+                throw new ArgumentException(nameof(outputPath));
+
+            return new FileStream(outputPath, FileMode.CreateNew, FileAccess.Write, FileShare.None);
+        }
+
+        private Stream _outputStream;
+    }
+}
